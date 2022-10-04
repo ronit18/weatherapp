30c0ce9dee197e3c23b870e3a2e506f904698a16
Ronit-ArchLinux <ronitgandhi96@gmail.com>
2022-10-04 10:22:33 +0530

    axios and reacticons install

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Head from "next/head";
+import axios from "axios";
+import { useState } from "react";
+import { BsSearch } from "react-icons/bs";
 
 export default function Home() {
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=${process.env.API_KEY}`;
 	return (
 		<div>
 			<Head>
